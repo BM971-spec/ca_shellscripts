f1fc923f954e9a9e7c4d920ade00c2a7a212d336
maniocbaptiste <baptiste.manioc2@gmail.com>
2024-11-05 19:26:32 +0100

    ajout Array.sh

---
+++ Array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NAMES=( John Eric Jessica )
+# write your code here
+NUMBERS=()
+STRINGS=()
+NumberOfNames=0
+STRING[0]=Hello
+STRING[1]=World
+NUMBERS[0]=1
+NUMBERS[1]=2
+NUMBERS[2]=3
+echo ${#NAMES[@]}
+echo ${NAMES[1]}
