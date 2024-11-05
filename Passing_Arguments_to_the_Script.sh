93aa67679eb697204f8291f8628e6a1f9005515e
BM971-spec <baptiste.manioc2@gmail.com>
2024-11-05 17:37:29 +0100

    Update Passing_Arguments_to_the_Script.sh

--- Passing_Arguments_to_the_Script.sh
+++ Passing_Arguments_to_the_Script.sh
@@ -2,15 +2,15 @@
 function File {
     # think you are inside the file
     # Change Here
-    echo 0
+    echo $#
 }
 
 # Do not change anything
-if [ ! 0 -lt 1 ]; then
-    File 
+if [ ! $# -lt 1 ]; then
+    File $*
     exit 0
 fi
 
 # change here
 # here you can pass the arguments
-bash Passing_Arguments_to_the_Script.sh Shell is fun
+bash prog.sh Shell is fun
