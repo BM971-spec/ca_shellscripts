baf8310f11a990a151bf94deebbc4b3ea0af065e
BM971-spec <baptiste.manioc2@gmail.com>
2024-11-06 20:06:12 +0100

    Update Array.sh

--- Array.sh
+++ Array.sh
@@ -5,10 +5,12 @@ NUMBERS=()
 STRINGS=()
 second_name=$(echo "${NAMES[1]}")
 NumberOfNames=0
-STRING[0]=Hello
-STRING[1]=World
+STRINGS[0]=Hello
+STRINGS[1]=World
 NUMBERS[0]=1
 NUMBERS[1]=2
 NUMBERS[2]=3
+echo ${STRINGS[@]}
 echo ${#NAMES[@]}
 echo $second_name
+echo ${#NAMES[@]}
