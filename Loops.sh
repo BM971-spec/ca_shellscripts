1184c3e9dc8fe294c20681e3a3ae2f35c988845a
BM971-spec <baptiste.manioc2@gmail.com>
2024-11-11 16:40:16 +0100

    Update Loops.sh

--- Loops.sh
+++ Loops.sh
@@ -3,16 +3,14 @@ NUMBERS=(951 402 984 651 360 69 408 319 601 485 980 507 725 547 544 615 83 165 1
 
 # write your code here
 for NUMBERS in "${NUMBERS[@]}"; do
-    # Vérifie si le nombre est pair
-    
-        if (( NUMBERS == 237 )); then
-        # Si le nombre est 10, arrêter la boucle
+   if (( NUMBERS == 237 )); then
         break
-        if (( NUMBERS % 2 == 1 )); then
-        # Si le nombre est pair, passer à l'élément suivant
-        continue
         fi
-    fi
-    # Afficher le nombre (si il est pair)
-    echo "Nombre pair : $NUMBERS"
+        
+        if (( NUMBERS % 2 == 1 )); then
+        continue
+        
+        fi
+    
+    echo $NUMBERS
     done
